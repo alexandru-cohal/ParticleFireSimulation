bcc54e67ccddd2cc5bc13eeec83159e7b5198dc8
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-04-25 18:23:00 +0200

    Window's color was animated by generating different values for the Red, Green and Blue values of all the pixels

--- src/mainApp.cpp
+++ src/mainApp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include <SDL.h>
 
 #include "Screen.h"
@@ -18,11 +19,17 @@ int main()
 	// The application loop
 	while (true)
 	{
-		// Set all the window's pixels to the purple color
+		// Generate the Red, Blue and Green values for the window's pixels
+		int elapsedTime = SDL_GetTicks();
+		unsigned char red = (unsigned char)((1 + sin(elapsedTime * 0.0001)) * 128);
+		unsigned char green = (unsigned char)((1 + sin(elapsedTime * 0.0002)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsedTime * 0.0003)) * 128);
+
+		// Set all the window's pixels
 		for (int y = 0; y < Screen::WINDOW_HEIGHT; y++)
 			for (int x = 0; x < Screen::WINDOW_WIDTH; x++)
 			{
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 
 		// Update the screen
