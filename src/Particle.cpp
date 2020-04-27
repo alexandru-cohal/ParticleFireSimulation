659c6e5568334253ef04dc1076fe6990270492f4
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-04-27 23:30:35 +0200

    Improvemend of the particle motion (curly movement, random reinitialization of the particles to the middle of the window, speed initialization changed)

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -8,11 +8,22 @@ namespace particuleSimulator
 	Particle::Particle(): m_x(0), m_y(0)
 	{
 		m_direction = (2 * M_PI * rand()) / RAND_MAX;
-		m_speed = (0.001 * rand()) / RAND_MAX;
+		m_speed = pow((0.05 * rand()) / RAND_MAX, 2);
+	}
+
+	void Particle::init()
+	{
+		m_x = 0;
+		m_y = 0;
+		m_direction = (2 * M_PI * rand()) / RAND_MAX;
+		m_speed = pow((0.05 * rand()) / RAND_MAX, 2);
 	}
 
 	void Particle::update(int intervalTime)
 	{
+		// Modify the direction for having a "curly" effect
+		m_direction += 0.001 * intervalTime;
+
 		// Calculate the speed in x and y directions
 		double xSpeed = m_speed * cos(m_direction);
 		double ySpeed = m_speed * sin(m_direction);
@@ -20,5 +31,17 @@ namespace particuleSimulator
 		// Update the position based on the speed and the time interval between updates
 		m_x += xSpeed * intervalTime;
 		m_y += ySpeed * intervalTime;
+
+		// If the particle goes outside of the window, reinitialize it in the middle of the window
+		if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1)
+		{
+			init();
+		}
+
+		// Choose randomly if the particle will be moved in the middle of the window
+		if (rand() < RAND_MAX / 1000)
+		{
+			init();
+		}
 	}
 }
--- src/Particle.h
+++ src/Particle.h
@@ -8,12 +8,17 @@ namespace particuleSimulator
 	public:
 		double m_x;
 		double m_y;
+
+	private:
 		double m_speed;
 		double m_direction;
 
 	public:
 		Particle();
 		void update(int intervalTime);
+
+	private:
+		void init();
 	};
 }
 
